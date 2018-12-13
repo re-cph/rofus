8919c6c22dd49d1497b106c29b6383de1e699406
oeb <omnia@re-cph.com>
2018-12-13 11:01:03 +0200

    Updating url for spillemyndigheden.dk

--- js/rofus_feed.js
+++ js/rofus_feed.js
@@ -35,7 +35,7 @@ $(document).ready(function() {
 
   }
 
-crossDomainAjax('https://spillemyndigheden.dk/json/rofus?_format=json', function (data) {
+crossDomainAjax('https://www.spillemyndigheden.dk/json/rofus?_format=json', function (data) {
     // success logic
    JSON2HTML(data);
 });
