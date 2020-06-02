b86654c328f67afc31c14f8c634392ddb15d7e5d
Christian Mehlbye <m@re-cph.com>
2020-06-02 16:30:14 +0200

    Webtilgængelighed: Move HTML out of js-file, in order to ensure HTML validation on ARIA labels

--- js/rofus_feed.js
+++ js/rofus_feed.js
@@ -4,9 +4,12 @@ $(document).ready(function() {
 
     // var monthNames = ['januar','februar', 'marts', 'april', 'maj', 'juni', 'juli', 'august', 'september', 'oktober', 'november', 'december'];
 
-    var news = '<div class="crown fr"></div>\n';
-    news += '<h2 id="news-header">Nyt om ROFUS fra spillemyndigheden.dk</h2>\n';
-    news += '<hr />\n';
+    //var news = '<div class="crown fr"></div>\n';
+    //news += '<h2 id="news-header">Nyt om ROFUS fra spillemyndigheden.dk</h2>\n';
+    //news += '<hr />\n';
+
+    var news = '';
+
 
     for (i = 0; i < nodes.length; i++) {
       var item = nodes[i];
