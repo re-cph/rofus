f7898e10270155c388003d798fd4a2c9a753487d
vesters <v@re-cph.com>
2025-05-16 15:05:33 +0200

    i18n basics working

---
+++ src/i18n/ui.ts
@@ -0,0 +1,22 @@
+export const languages = {
+  da: "Dansk",
+  kl: "Kalaallisut",
+  en: "English",
+};
+
+export const defaultLang = "da";
+
+export const ui = {
+  da: {
+    "nav.home": "Hej",
+    "nav.about": "Om",
+  },
+  kl: {
+    "nav.home": "xxx",
+    "nav.about": "yyy",
+  },
+  en: {
+    "nav.home": "Home",
+    "nav.about": "About",
+  },
+} as const;
