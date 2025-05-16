700cebf3e1e7a973d59adb5af97b46962d591c4e
vesters <v@re-cph.com>
2025-05-16 15:44:52 +0200

    Floating login component translateable

--- src/i18n/ui.ts
+++ src/i18n/ui.ts
@@ -8,15 +8,23 @@ export const defaultLang = "da";
 
 export const ui = {
   da: {
+    "floatingLogin.tagline": "Log på ROFUS med MitID",
+    "floatingLogin.title":
+      "I ROFUS kan du frivilligt udelukke dig selv fra onlinespil, fysiske kasinoer og væddemål i fysiske butikker i Danmark.",
     "nav.home": "Hej",
     "nav.about": "Om",
+    "floatingLogin.button": "Registrér med MitID",
+    "floatingLogin.list1": "Tag en 24 timers spillepause",
+    "floatingLogin.list2": "Midlertidig udelukkelse i 1, 3 eller 6 måneder",
+    "floatingLogin.list3": "Endelig udelukkelse",
+    "floatingLogin.disclaimer":
+      "Vær opmærksom på, at Spillemyndigheden tager forbehold for eventuelle tekniske fejl i ROFUS og kan således ikke holdes ansvarlig for dette",
   },
   kl: {
-    "nav.home": "xxx",
-    "nav.about": "yyy",
+    "floatingLogin.tagline": "Ding dong",
+    "floatingLogin.disclaimer": "Ding dong",
   },
   en: {
-    "nav.home": "Home",
-    "nav.about": "About",
+    "floatingLogin.tagline": "Ding dong",
   },
 } as const;
