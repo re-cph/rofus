3ff7a367220cb86b432e21eb582c5fe40db72feb
vesters <v@re-cph.com>
2025-05-16 16:21:30 +0200

    SectionInfo POC

--- src/i18n/ui.ts
+++ src/i18n/ui.ts
@@ -19,6 +19,33 @@ export const ui = {
     "floatingLogin.list3": "Endelig udelukkelse",
     "floatingLogin.disclaimer":
       "Vær opmærksom på, at Spillemyndigheden tager forbehold for eventuelle tekniske fejl i ROFUS og kan således ikke holdes ansvarlig for dette",
+    "sectionInfo.title":
+      "ROFUS er Spillemyndighedens register\nover frivilligt udelukkede spillere",
+    "sectionInfo.section1.ariaLabel": "funktion",
+    "sectionInfo.section1.question": "Hvordan virker ROFUS?",
+    "sectionInfo.section1.answer":
+      "Er du registreret i ROFUS, er du udelukket fra onlinespil, alle fysiske kasinoer og fysiske væddemål i butikker i Danmark.\nDu vælger selv hvor længe",
+    "sectionInfo.section2.ariaLabel": "fortrolighed",
+    "sectionInfo.section2.question": "100% fortroligt",
+    "sectionInfo.section2.answer":
+      "Spillemyndigheden videregiver ikke oplysninger om din registrering til andre, med mindre du selv forsøger at spille i din udelukkelsesperiode, eller hvis en tilladelsesindehaver forsøger at sende direkte markedsføring til dig i din udelukkelsesperiode.",
+    "sectionInfo.section3.ariaLabel": "omfattet",
+    "sectionInfo.section3.question": "Hvilke spil er omfattet?",
+    "sectionInfo.section3.answer":
+      "Onlinespil med indsats udbudt af spiludbydere med tilladelse fra Spillemyndigheden samt alle fysiske kasinoer og fysiske væddemål i butikker i Danmark.\n\n Se en liste over onlinespiludbydere \n\n Se en liste over fysiske kasinoer",
+    "sectionInfo.section3.link1.text": "Se en liste over onlinespiludbydere",
+    "sectionInfo.section3.link1.href":
+      "https://spillemyndigheden.dk/tilladelsesindehavere",
+    "sectionInfo.section3.link2.text": "Se en liste over fysiske kasinoer",
+    "sectionInfo.section3.link2.href":
+      "https://spillemyndigheden.dk/liste-over-kasinoer-i-danmark",
+    "sectionInfo.section4.ariaLabel": "fortrydelse",
+    "sectionInfo.section4.question": "Hvad hvis jeg fortryder?",
+    "sectionInfo.section4.answer":
+      "Styrken ved ROFUS er, at din registrering ikke kan annulleres - også selvom du fortryder.",
+    "sectionInfo.cta": "Registrér dig i ROFUS med MitID",
+    "sectionInfo.notaBene":
+      "Vær opmærksom på, at Spillemyndigheden tager forbehold for eventuelle tekniske fejl i ROFUS og kan således ikke holdes ansvarlig for dette",
   },
   kl: {
     "floatingLogin.tagline": "Ding dong",
